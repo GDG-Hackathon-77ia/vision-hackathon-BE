116dfe13c3cebf8ac952500f83723fbcbe23e828
gitjiho <jiho99322@gmail.com>
2024-11-08 23:20:10 +0900

    feat: set diary entity

--- src/main/java/com/gdg/kkia/storage/diary/entity/Diary.java
+++ src/main/java/com/gdg/kkia/storage/diary/entity/Diary.java
@@ -1,4 +1,30 @@
 package com.gdg.kkia.storage.diary.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Diary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String content;
+    @NotNull
+    @CreatedDate
+    private LocalDateTime writtenDatetime;
+
 }
