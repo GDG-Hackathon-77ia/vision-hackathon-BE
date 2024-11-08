80e383a938f1d50c750a4ff3272df6a7f2e20d4b
gitjiho <jiho99322@gmail.com>
2024-11-08 22:54:16 +0900

    feat: set pet entity

--- src/main/java/com/gdg/kkia/pet/entity/Pet.java
+++ src/main/java/com/gdg/kkia/pet/entity/Pet.java
@@ -1,4 +1,34 @@
 package com.gdg.kkia.pet.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String name;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Pet.Level level;
+    @NotNull
+    private int experience;
+
+    public enum Level {
+        BABY,
+        KID,
+        STUDENT,
+        ADULT
+
+    }
+
 }
