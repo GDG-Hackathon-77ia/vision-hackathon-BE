187002e5a976395fc8424cba32817e71b7e0a6c4
gitjiho <jiho99322@gmail.com>
2024-11-08 22:39:36 +0900

    set: new setting

---
+++ src/main/java/com/gdg/kkia/KkiaApplication.java
@@ -0,0 +1,13 @@
+package com.gdg.kkia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdg/kkia/KkiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdg.kkia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
