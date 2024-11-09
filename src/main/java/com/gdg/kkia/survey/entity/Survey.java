0e3f9811e757b1a01ed991a181efc63eec58334d
gitjiho <jiho99322@gmail.com>
2024-11-10 02:10:27 +0900

    feat: implement survey logic

--- src/main/java/com/gdg/kkia/survey/entity/Survey.java
+++ src/main/java/com/gdg/kkia/survey/entity/Survey.java
@@ -1,4 +1,4 @@
-package com.gdg.kkia.servey.entity;
+package com.gdg.kkia.survey.entity;
 
 import com.gdg.kkia.member.entity.Member;
 import jakarta.persistence.*;
@@ -10,6 +10,8 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -18,18 +20,35 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
-public class Servey {
+public class Survey {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotNull
-    private String answer;
+    @ElementCollection
+    @CollectionTable(name = "survey_answers", joinColumns = @JoinColumn(name = "survey_id"))
+    @Column(name = "answer")
+    private List<Integer> answer = new ArrayList<>();
     @CreatedDate
     private LocalDateTime surveyedDatetime;
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Survey.Role role;
     @ManyToOne
     @JoinColumn(name = "member_id")
     @NotNull
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Member member;
+
+    public Survey(List<Integer> answer, Role role, Member member) {
+        this.answer = answer;
+        this.role = role;
+        this.member = member;
+    }
+
+    public enum Role {
+        USER,
+        MODEL
+    }
 }
